f98f0d931188722f0f9df7ac16037b1b3650394a
Neeraj Kumar <5895556-NeerajKumar24@users.noreply.replit.com>
2024-04-12 13:56:07 +0000

    Final commit

--- src/DFA.py
+++ src/DFA.py
@@ -1,4 +1,200 @@
 import graphviz as gv
+import shutil
+import os
+import random
+import moviepy.video.io.ImageSequenceClip
+
+# def validateInput(startState,finalStates,transitionfn,msg):
+
+#   transition_table = []
+#   for key, val in transitionfn.items():
+#       row = key.split("-")
+#       row.append(val)
+#       transition_table.append(row)
+#       row = []
+
+#   print("transition table", transition_table)
+#   dot = gv.Digraph("DFA", comment="DFA")
+#   dot.attr("node", shape="circle")
+
+#   # Deifne invisible node for starting point
+#   dot.node("", style="invis")
+
+#   # final nodes
+#   for node in finalStates:
+#       dot.node(node, shape="doublecircle")
+
+#   # define edges
+#   # define starting point
+#   dot.edge("", startState)
+
+
+#   traversed = []
+
+
+#   currentState = startState
+#   nextState = startState
+#   for char in msg:
+#     nextState = transitionfn[f'{currentState}-{char}']
+#     if([currentState,char,nextState] not in traversed):
+#       dot.edge(currentState, nextState, char,color="red")
+#       traversed.append([currentState,char,nextState])
+#     # print(currentState, char, nextState)
+
+#     currentState = nextState
+
+#   dot.node(nextState,color="red")
+
+#   for current_node, input_symbol, next_node in transition_table:
+#     if([current_node,input_symbol,next_node] in traversed):
+#       continue
+#     dot.edge(current_node, next_node, input_symbol)
+
+
+#   dot.render(directory='./src/static/img', view=False, format="png", engine="circo")
+
+#   print(nextState,"|||",finalStates)
+#   if(nextState in finalStates):
+#     return f"{msg} String Accepted"
+
+#   return f"{msg} String Rejected"
+
+
+def validateInput(startState, finalStates, transitionfn, msg):
+
+    transition_table = []
+    for key, val in transitionfn.items():
+        row = key.split("-")
+        row.append(val)
+        transition_table.append(row)
+        row = []
+
+    # print("transition table", transition_table)
+
+    dot = gv.Digraph("DFA", comment="DFA")
+    dot.attr("node", shape="circle")
+
+    # Deifne invisible node for starting point
+    dot.node("", style="invis")
+
+    # final nodes
+    for node in finalStates:
+        dot.node(node, shape="doublecircle")
+
+    # define edges
+    # define starting point
+    dot.edge("", startState)
+
+    if os.path.exists("src/static/img"):
+      shutil.rmtree("src/static/img")
+
+    if os.path.exists("src/static/video"):
+      for file in os.listdir("src/static/video"):
+        os.remove("src/static/video/" + file)
+
+    currentState = startState
+    nextState = startState
+    for index, char in enumerate(msg):
+        nextState = transitionfn[f"{currentState}-{char}"]
+        currentState = nextState
+
+    color = 'red'
+    if(nextState in finalStates):
+      color = 'green'
+
+    currentState = startState
+    nextState = startState
+    for index, char in enumerate(msg):
+        nextState = transitionfn[f"{currentState}-{char}"]
+        # if([currentState,char,nextState] not in traversed):
+        createDFAImg(
+            transitionfn, startState, finalStates, currentState, nextState, char, index, color
+        )
+        # print(currentState, char, nextState)
+
+        currentState = nextState
+
+    for current_state, input_symbol, next_state in transition_table:
+        dot.edge(current_state, next_state, input_symbol)
+
+    dot.node(nextState, color=color)
+
+    dot.render(
+        directory="src/static/img",
+        view=False,
+        format="png",
+        engine="circo",
+        filename=f"img{len(msg)}",
+    )
+
+    temp_rn = random.randint(1000000, 9999999)
+    output_video_file_name = f"src/static/video/video-{temp_rn}.mp4"
+    image_folder = "src/static/img"
+
+    image_files = [
+        os.path.join(image_folder, img)
+        for img in os.listdir(image_folder)
+        if img.endswith(".png")
+    ]
+
+    image_files.insert(0, "src/static/DFA.gv.png")
+    # print(image_files)
+    clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=1)
+    clip.write_videofile(output_video_file_name)
+    return output_video_file_name
+
+
+def createDFAImg(
+    data, startnode, finalnodes, currentState, nextState, currentSymbol, index, color
+):
+    # preprocessing
+    transition_table = []
+    for key, val in data.items():
+        row = key.split("-")
+        row.append(val)
+        transition_table.append(row)
+        row = []
+
+    # print("transition table", transition_table)
+
+    dot = gv.Digraph("DFA", comment="DFA")
+    dot.attr("node", shape="circle")
+
+    # Deifne invisible node for starting point
+    dot.node("", style="invis")
+
+    # final nodes
+    for node in finalnodes:
+        dot.node(node, shape="doublecircle")
+
+    # define edges
+    # define starting point
+    dot.edge("", startnode)
+
+    for current_state, input_symbol, next_state in transition_table:
+        if current_state == currentState:
+            dot.node(currentState, color=color)
+        if next_state == nextState:
+            dot.node(nextState, color=color)
+        if (
+            input_symbol == currentSymbol
+            and nextState == next_state
+            and currentState == current_state
+        ):
+
+            dot.edge(current_state, next_state, input_symbol, color=color)
+        else:
+            dot.edge(current_state, next_state, input_symbol)
+
+    # print(dot.source)
+
+    dot.render(
+        directory="src/static/img",
+        view=False,
+        format="png",
+        engine="circo",
+        filename=f"img{index}",
+    )
 
 
 def createDFA(data, startnode, finalnodes, output_dir):
@@ -10,7 +206,7 @@ def createDFA(data, startnode, finalnodes, output_dir):
         transition_table.append(row)
         row = []
 
-    print("transition table", transition_table)
+    # print("transition table", transition_table)
 
     dot = gv.Digraph("DFA", comment="DFA")
     dot.attr("node", shape="circle")
@@ -29,13 +225,19 @@ def createDFA(data, startnode, finalnodes, output_dir):
         dot.edge(current_state, next_state, input_symbol)
     print(dot.source)
 
-    dot.render(directory=output_dir, view=False, format="png", engine="circo")
+
+    dot.render(
+        directory=output_dir,
+        view=False,
+        format="png",
+        engine="circo",
+    )
 
 
 # startnode = "0"
 # currentState, inputSymbol, nextstate
 # data = {
-#     ("0-0"): "1", 
+#     ("0-0"): "1",
 #     ("0-1"): "2",
 #     ("1-0"): "1",
 #     ("1-1"): "0",
--- src/static/index.js
+++ src/static/index.js
@@ -1,15 +1,20 @@
-document.getElementById("test").textContent = "testing"
-
-
-
-
 document.getElementById("alphabetBtn").addEventListener("click", function(e){
   e.preventDefault();
-
+  const stateTable = document.getElementById("stateTable");
   var alpha = document.getElementById("alphabets").value;
   var alphabets = alpha.split(",");
-
-  document.getElementById("confirmedAlphabet").textContent = alphabets;
+  // alphabets.unshift("States")
+  const tr = document.createElement("tr");
+  const st = document.createElement("td");
+  st.textContent = "States";
+  tr.appendChild(st)
+  for (element of alphabets) {
+    const td = document.createElement('td');
+    td.textContent = element;
+    tr.appendChild(td);
+  }
+  stateTable.appendChild(tr);
+  // document.getElementById("confirmedAlphabet").textContent = alphabets;
 
   document.getElementById("alphabetContainer").style.display = "none";
   genrateTransitionTable(alphabets,0);
@@ -19,9 +24,9 @@ let states = [];
 let newStates = ["q0"];
 
 function genrateTransitionTable(alphabets,noOfStates) {
-  // let newStates = ['q1'];
-  // let noOfStates = 0;
-  // let it = 0;
+
+  const tr = document.createElement("tr");
+
 
   if(newStates.length == 0) {
     div = document.createElement("div");
@@ -30,16 +35,18 @@ function genrateTransitionTable(alphabets,noOfStates) {
     subBtn.textContent = 'Submit';
     document.getElementById("statesInput").appendChild(div);
     
-    cbContainer = document.createElement("fielset");
+    cbContainer = document.createElement("fieldset");
     legend = document.createElement("legend");
     legend.textContent = "Final States";
+
+    cbContainer.appendChild(legend);
     
     for (let index = 0; index < states.length; index++) {
       label = document.createElement("label");
       label.textContent = `${states[index]}`;
       cb = document.createElement('input');
-      cb.type = 'checkbox';
-      cb.name = "finalstate";
+      cb.type = 'checkbox';    
+      cb.name = "finalstate-"+index;
       cb.value = states[index];
       label.appendChild(cb);
       cbContainer.appendChild(label);
@@ -54,20 +61,27 @@ function genrateTransitionTable(alphabets,noOfStates) {
   let state = newStates.shift();
   states.push(state);
   let nextStates = [];
-
-  div = document.createElement('div');
+  const tdSt = document.createElement('td');
+  // div = document.createElement('div');
   p = document.createElement('p');
   p.textContent = `${state}`;
-  div.appendChild(p);
+  tdSt.appendChild(p);
+  tr.appendChild(tdSt);
   for(let i = 0; i < alphabets.length; i++){
+    const tdIn = document.createElement('td');
     let inp = document.createElement('input');
+    inp.placeholder = "Enter next state";
     inp.type = "text"
     inp.name = `${state}-${alphabets[i]}`
-    div.appendChild(inp);
+    tdIn.appendChild(inp);
+    tr.appendChild(tdIn);
   }
   const proceedBtn = document.createElement("button");
   proceedBtn.type = "button";
   proceedBtn.textContent = "Proceed";
+
+  const tdBtn = document.createElement('td');
+  tdBtn.appendChild(proceedBtn);
 
 
   proceedBtn.addEventListener("click", function (e) {
@@ -94,7 +108,8 @@ function genrateTransitionTable(alphabets,noOfStates) {
     genrateTransitionTable(alphabets,states.length);
 
   });
-  div.appendChild(proceedBtn);
-  document.getElementById("statesInput").appendChild(div);
+  tr.appendChild(tdBtn);
+  // document.getElementById("statesInput").appendChild(div);
+  document.getElementById("stateTable").appendChild(tr)
 
 }
